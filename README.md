84172c00132a705a40f7649ba20e1bfa24c45613
Oscar <oscar.vegaherrera@emeal.nttdata.com>
2026-08-14 15:19:34 +0200

    Render Map

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Levantar el proyecto en Dev
 2. Instalar dependencias.
 3. Generar el `.env` basado en el `.env.template`.
 4. Ejecutar el comando `npm run set-envs`
+5. URL de servicio externo: https://openfreemap.org/quick_start/
--- src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -1,9 +1,34 @@
-import { Component } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, viewChild } from "@angular/core";
+import {Map, setWorkerUrl} from 'maplibre-gl';
+
+declare const maplibregl: any;
 
 
 @Component({
   selector: 'fullscreen',
-  templateUrl: './fullscreen-map-page.component.html'
+  templateUrl: './fullscreen-map-page.component.html',
+  styles: `
+    div {
+      widht: 100vw;
+      height: calc(100vh - 64px);
+    }
+  `
 })
 
-export class FullScreenMapPageComponent {}
+export class FullScreenMapPageComponent implements AfterViewInit {
+  @ViewChild('map') mapContainer!: ElementRef<HTMLElement>;
+
+  ngAfterViewInit(): void {
+    if (!this.mapContainer.nativeElement) {
+      return;
+    }
+
+    const map = new maplibregl.Map({
+      container: this.mapContainer?.nativeElement,
+      style: 'https://tiles.openfreemap.org/styles/liberty', 
+      center: [-6.3539, 36.7789], // Madrid, España [Longitud, Latitud]
+      zoom: 9
+    });
+  }
+  
+}
